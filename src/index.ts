50400fa2c2cb424a2519f6c7413787986fd8b1a4
Kristyna Petrlikova <petrlik@matfyz.cz>
2024-08-25 02:19:46 +0200

    add basic data parsing

--- src/index.ts
+++ src/index.ts
@@ -1,28 +1,73 @@
 import Ajv from 'ajv';
 import addFormats from 'ajv-formats';
-import vegaSchema from 'vega/build/vega-schema.json';
-import jsonSchema from 'ajv/dist/refs/json-schema-draft-06.json'
+//import vegaSchema from 'vega/build/vega-schema.json';
+//import jsonSchema from 'ajv/dist/refs/json-schema-draft-06.json'
 import * as fs from 'fs';
+import * as vega from 'vega';
 
-
-const ajv = new Ajv({
-  strict: false,
-});
-
-addFormats(ajv);
-ajv.addMetaSchema(jsonSchema);
-
-const vegaValidator = ajv.compile(vegaSchema);
-
-function validateVega(spec: string){
-  const valid = vegaValidator(spec);
-  console.log(valid) 
+function isSignal(x: any): boolean {
+  return x && x.signal;
 }
 
-function greeter(person: string){
-  return "Hello, " + person;
+function isObject(x: any): x is {[index: string]: any} {
+  return x === Object(x)
 }
 
-let user = "Jane User"
+function toArray(x: any): any[] {
+  return x != null ? (Array.isArray(x) ? x : [x]) : [];
+}
 
-document.body.textContent = greeter(user);
+function hasSignal(x: any) {
+  if (isSignal(x))
+    return true;
+  if (isObject(x))
+    for (const key in x) {
+      if (hasSignal(x[key]))
+        return true;
+    }
+  return false;
+}
+
+async function loadDataset(dataset: any){
+  let loader = vega.loader()
+  let name: string = ""
+  let out: any
+  if(dataset.values){
+    if(isSignal(dataset.values) || hasSignal(dataset.format)){
+      vega.error("Signals aren't supported")
+    }
+    else {
+      name = dataset.name
+      out = vega.read(dataset.values, dataset.format)
+    }
+  }
+  else if(dataset.url){
+    if(hasSignal(dataset.url) || hasSignal(dataset.format)){
+      vega.error("Signals aren't supported")
+    }
+    else{
+      name = dataset.name
+      await loader.load(dataset.url).then(function(ds){
+          out = vega.read(ds, dataset.format)
+          console.log(out)
+      });
+    }
+  }
+  else if(dataset.source){
+    vega.error("not implemented yet")
+  }
+  console.log(name)
+  out = out.map(function(val: any){
+    if(!isObject(val)){
+      return {"data": val}
+    }
+    return val
+    })
+  console.log(out)
+}
+
+let rawSpec = fs.readFileSync(0, 'utf-8');
+let parsedSpec = JSON.parse(rawSpec)
+
+
+toArray(parsedSpec.data).forEach(x => loadDataset(x))
